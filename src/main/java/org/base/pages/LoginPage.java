6a320cefd55faaf3e60395fd45bfff5837b0be56
MarianStasiuk <mst@primexm.com>
2025-03-24 20:01:10 +0200

    lesson 6 custom framework

--- src/main/java/org/base/pages/LoginPage.java
+++ src/main/java/org/base/pages/LoginPage.java
@@ -1,24 +1,29 @@
 package org.base.pages;
 
+import org.base.config.PageTools;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static org.base.helpers.CustomConditions.*;
 
-public class LoginPage {
+public class LoginPage extends PageTools {
     private final String usernameInput = "//input[@placeholder='Username']";
     private final String passwordInput = ".input_error.form_input[id=password]";
     private final String loginButton = "login-button";
 
     public void typeUsername(String username) {
-        $(byXpath(usernameInput)).shouldBe(inputCondition).append(username);
+        should("xpath", inputCondition, usernameInput);
+        type("xpath", username, usernameInput);
     }
 
     public void typePassword(String password) {
-        $(byCssSelector(passwordInput)).shouldBe(inputCondition).append(password);
+        should("css", inputCondition, passwordInput);
+        type("css", password, passwordInput);
     }
 
     public void clickLoginButton() {
-        $(byId(loginButton)).shouldBe(clickable).submit();
+        should("id", clickable, loginButton);
+        click("id", loginButton);
     }
 }
